0002a9050326fa4957d985ec3f572caf6567adbd
DailyPS <55373035+DailyPS@users.noreply.github.com>
2022-10-11 18:24:51 +0900

    test.js for W07

--- lib.test.js
+++ lib.test.js
@@ -5,10 +5,10 @@ test("sum([1, 2]) should be 3", () => {
     expect(lib.sum([1, 2])).toBe(3);
 })
 
-test("sum([1, 2]) should be 3", () => {
-    expect(lib.sum([1, 2])).toBe(3);
+test("avg([-5, 5]) should be 0", () => {
+    expect(lib.sum([-5, 5])).toBe(0);
 })
 
-test("sum([1, 2]) should be 3", () => {
-    expect(lib.sum([1, 2])).toBe(3);
+test("sum([0, 3, 2]) should be 3", () => {
+    expect(lib.sum([0, 3, 2])).toBe(3);
 })
